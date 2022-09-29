2b1186be2838bf34eda29d0d08ec7922c2628df2
KyawLwinSoe27 <soek38902@gmail.com>
2022-09-29 21:10:27 +0630

    new_feature: update text in app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ const App = () => {
   return (
     <>
       <h1>This is for Host Testing App</h1>
+      <h2>This is for new features</h2>
+      <h3> Reat Deployment successful</h3>
+      <h4>Thanks for using my app</h4>
     </>
   )
 }
